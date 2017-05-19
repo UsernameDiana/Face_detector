844419480523536952a42c32801b7af219d730d1
matyasfeher <fehermatyas17@gmail.com>
2017-05-19 10:11:18 +0200

    resize.py so far so good

--- facedetect/sample_images/resize.py
+++ facedetect/sample_images/resize.py
@@ -1,21 +1,16 @@
 import cv2
 
-faceCascade = cv2.CascadeClassifier('../haarcascades/haarcascade_frontalcatface_default.xml')
-image = cv2.imread('../sample_images/1.jpg')
+faceCascade = cv2.CascadeClassifier('../../haarcascades/haarcascade_frontalface_default.xml')
+
+image = cv2.imread('../sample_images/3.jpg')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-faces = faceCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.1,
-    minNeighbors=5,
-    minSize=(30, 30),
-    flags = cv2.cv.CV_HAAR_SCALE_IMAGE
-)
 
+faces = faceCascade.detectMultiScale(gray, 1.3, 5)
+print(len(faces))
 for (x, y, w, h) in faces:
     cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-#.....to be continued
 cv2.imshow("Faces found" ,image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
