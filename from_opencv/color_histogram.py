0d33910b32da8791083b90ca6bcc7be746676ee7
matyasfeher <fehermatyas17@gmail.com>
2017-05-19 12:17:39 +0200

    histogram loop

--- from_opencv/color_histogram.py
+++ from_opencv/color_histogram.py
@@ -1,19 +1,21 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+images = ['../facedetect/images_resize/face_of_1.jpg','../facedetect/images_resize/face_of_2.jpg','../facedetect/images_resize/face_of_3.jpg']
+for image in images:
+    img = cv2.imread(image, -1)
+    cv2.imshow('1',img)
 
-img = cv2.imread('../facedetect/images_resize/face_of_1.jpg', -1)
-cv2.imshow('1',img)
-
-color = ('b','g','r')
-for channel,col in enumerate(color):
-    histr = cv2.calcHist([img],[channel],None,[256],[0,256])
-    plt.plot(histr,color = col)
-    plt.xlim([0,256])
-plt.title('Histogram for color scale picture')
-plt.show()
+    color = ('b','g','r')
+    for channel,col in enumerate(color):
+        histr = cv2.calcHist([img],[channel],None,[256],[0,256])
+        plt.plot(histr,color = col)
+        plt.xlim([0,256])
+    plt.title('Histogram for color scale picture')
+    plt.show()
 
 while True:
-    k = cv2.waitKey(0) & 0xFF     
-    if k == 27: break             # ESC key to exit 
+    k = cv2.waitKey(0) & 0xFF
+    if k == 27:
+        break             # ESC key to exit
 cv2.destroyAllWindows()
--- from_opencv/gray_histogram.py
+++ from_opencv/gray_histogram.py
@@ -1,13 +1,14 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
-
-gray_img = cv2.imread('../facedetect/images_resize/face_of_1.jpg', cv2.IMREAD_GRAYSCALE)
-cv2.imshow('1',gray_img)
-hist = cv2.calcHist([gray_img],[0],None,[256],[0,256])
-plt.hist(gray_img.ravel(),256,[0,256])
-plt.title('Histogram for gray scale picture')
-plt.show()
+images = ['../facedetect/images_resize/face_of_1.jpg','../facedetect/images_resize/face_of_2.jpg','../facedetect/images_resize/face_of_3.jpg']
+for image in images:
+    gray_img = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
+    cv2.imshow('1',gray_img)
+    hist = cv2.calcHist([gray_img],[0],None,[256],[0,256])
+    plt.hist(gray_img.ravel(),256,[0,256])
+    plt.title('Histogram for gray scale picture')
+    plt.show()
 
 while True:
     k = cv2.waitKey(0) & 0xFF     
