cbcfd119113776a7756afc3d8013be36d706e48b
matyasfeher <fehermatyas17@gmail.com>
2017-05-19 10:57:41 +0200

    resize.py is done

--- facedetect/sample_images/resize.py
+++ facedetect/sample_images/resize.py
@@ -1,16 +1,18 @@
 import cv2
 
 faceCascade = cv2.CascadeClassifier('../../haarcascades/haarcascade_frontalface_default.xml')
+images = ['../sample_images/1.jpg','../sample_images/2.jpg','../sample_images/3.jpg']
 
-image = cv2.imread('../sample_images/3.jpg')
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+for img in images:
+    print(img)
+    image = cv2.imread(img)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+    faces = faceCascade.detectMultiScale(gray, 1.3, 5)
 
-faces = faceCascade.detectMultiScale(gray, 1.3, 5)
-print(len(faces))
-for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        print(x, y, x+w, y+h)
+        new_img = image[y:y+h, x:x+w]
+        cv2.imwrite(img, new_img)
 
-cv2.imshow("Faces found" ,image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
