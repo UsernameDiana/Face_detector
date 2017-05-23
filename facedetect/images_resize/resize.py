16f18bfe35173d6e653987a256cfaf6a159d1565
matyasfeher <fehermatyas17@gmail.com>
2017-05-23 12:54:04 +0200

    matching faces in progress

--- facedetect/images_resize/resize.py
+++ facedetect/images_resize/resize.py
@@ -4,7 +4,6 @@ faceCascade = cv2.CascadeClassifier('../../haarcascades/haarcascade_frontalface_
 images = ['1.jpg','2.jpg','3.jpg']
 
 for img in images:
-    print(img)
     image = cv2.imread(img)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
@@ -12,7 +11,6 @@ for img in images:
 
     for (x, y, w, h) in faces:
         cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        print(x, y, x+w, y+h)
         new_img = image[y:y+h, x:x+w]
         cv2.imwrite('face_of_'+img, new_img)
 
--- facedetect/my_face_detect.py
+++ facedetect/my_face_detect.py
@@ -3,24 +3,25 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier('../haarcascades/haarcascade_frontalface_default.xml')
 
-eye_cascade = cv2.CascadeClassifier('../haarcascades/haarcascade_eye.xml')
+matyas = cv2.imread('images_resize/face_of_2.jpg')
+gray_matyas = cv2.cvtColor(matyas, cv2.COLOR_BGR2GRAY)
 
 cap = cv2.VideoCapture(1)
-
+match = []
 while True:
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
     for (x,y,w,h) in faces:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,255),1)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
-        
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-        for (ex,ey,ew,eh) in eyes:
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
+        resized = cv2.resize(roi_gray, (83, 83), interpolation=cv2.INTER_CUBIC)
+        print(type(roi_gray), resized.shape)
+        cv2.imwrite('captured_resized_face.jpg', resized)
+        cap_face = cv2.imread('captured_reized_face.jpg')
     cv2.imshow('img',img)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
