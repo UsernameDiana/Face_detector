e38f12aa5ef088d8afd59d041397ce7666d08999
stinaanita <stina-stud@live.dk>
2017-05-24 10:41:05 +0200

    n

--- facedetect/face_templating.py
+++ facedetect/face_templating.py
@@ -9,19 +9,18 @@ template = cv2.imread('images_resize/face_of_1.jpg')
 template_gray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
 print(template_gray.shape)
 
-w, h = template_gray.shape
+w, h = template_gray.shape[::-1]
 count = 0
 
 # comparison in a list, matching a detected face with the templates
 res = cv2.matchTemplate(img_gray, template_gray, cv2.TM_CCOEFF_NORMED)
 threshold = 0.4
-print("RES ", res)
 loc = np.where( res >= threshold)
-print("LOC ", loc)
 
 for pt in zip(*loc[::-1]): # points which have values greater than threshold.
     count += 1
-    bla = cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
 
-print(count)
+
+print("Total Points Detected: {}".format(count))
 cv2.imshow('Detected',img_rgb)
