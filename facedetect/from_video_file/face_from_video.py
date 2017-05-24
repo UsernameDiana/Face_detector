5c72c117945b8599dbd6795ac6e6bfa51cf504fb
UsernameDiana <ms.strele@gmail.com>
2017-05-24 10:09:23 +0200

    added nececcary classes for using facedetect on video file

--- facedetect/from_video_file/face_from_video.py
+++ facedetect/from_video_file/face_from_video.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
 
     match = []
     while True:
-        img = cap.read()
+        ret, img = cap.read()
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
